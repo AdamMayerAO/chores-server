191a1d21a258eac10893b3053f807ba8529e1426
Adam Mayer <r.adammayer@gmail.com>
2021-05-13 21:37:18 -0400

    databaseurl Vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL
+    DATABASE_URL: 'postgres://ueyiglrh:OsZghd9-Jg6-x4W6cvT37GdFHELB3yAO@queenie.db.elephantsql.com:5432/ueyiglrh'  || `postrgresql://amayer@localhost/chores`
+
   }
