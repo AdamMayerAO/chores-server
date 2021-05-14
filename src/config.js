53bdf04776769f067f133ae020ff557a7e1edeae
Adam Mayer <r.adammayer@gmail.com>
2021-05-13 20:36:07 -0400

    database_url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: `postrgresql://amayer@localhost/chores`
+    DATABASE_URL: 'postgres://yeutiyemoqtqlh:dc05a1c50bebe4da790f2659ffa0e559b3be38429a53d1073a414bb49e1aa386@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d4h1fe625k7gvv'
+
+    //`postrgresql://amayer@localhost/chores`
   }
