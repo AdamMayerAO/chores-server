7e112843ea74fd0e56b723154368eff6a2a8d3d9
Adam Mayer <r.adammayer@gmail.com>
2021-05-16 15:59:51 -0400

    updated config.js file with correct order

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT:  8000 ||process.env.PORT,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: `postrgresql://amayer@localhost/chores`||'postgres://ueyiglrh:OsZghd9-Jg6-x4W6cvT37GdFHELB3yAO@queenie.db.elephantsql.com:5432/ueyiglrh'  
+    DATABASE_URL: 'postgres://ueyiglrh:OsZghd9-Jg6-x4W6cvT37GdFHELB3yAO@queenie.db.elephantsql.com:5432/ueyiglrh'  || `postrgresql://amayer@localhost/chores`
   }
